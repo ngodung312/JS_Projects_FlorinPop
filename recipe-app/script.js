744b86c4dca078dadf3292a162e74a7e5c33a689
APAC\GDL1HC <fixed-term.Dung.NgoLeHanh@vn.bosch.com>
2022-06-10 11:40:31 +0700

    Update 0.1: 10/06/2022

---
+++ recipe-app/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  // Owl-carousel
+  $(".fav-container .owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: false,
+    dots: false,
+    items: 4.5,
+  });
+});
